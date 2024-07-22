e7830b44097619c736672bce469d7383ed31303b
felixkad-lab <felix.amankonadiawuo@gmail.com>
2024-07-22 15:21:04 -0500

    Descriptive stats for electricity data

--- electricity_descriptive_stats.py
+++ electricity_descriptive_stats.py
@@ -11,7 +11,8 @@ def do_elec_descriptive_stats(prefix=None, df_util=None, top_up=None):
         f'output/{prefix}_{str(fig_ctr).zfill(2)}_raw_and_fixed.png'
     )       
     pf.plot_raw_and_fixed_credits(
-        df=df_util, x_lim=x_lim, y_lim=y_lim, xtick_step=20, figsize=[10, 12], figname=figname
+        df=df_util, x_lim=x_lim, y_lim=y_lim, xtick_step=20, figsize=[10, 12], 
+        figname=figname
     )
         
     # Make a countplot of measured and calculated values   
@@ -21,7 +22,9 @@ def do_elec_descriptive_stats(prefix=None, df_util=None, top_up=None):
         f'output/{prefix}_{str(fig_ctr).zfill(2)}_measured_countplot.png'
     )
     pf.make_countplot(
-        data=df_util, x='measured', hue=None, stat='count', palette=None, saturation=1, title=title, xlabel='Measurement', ylabel='Count', figsize=[8, 6], add_bar_labels=True, hue_order=None, figname=figname
+        data=df_util, x='measured', hue=None, stat='count', palette=None, 
+        saturation=1, title=title, xlabel='Measurement', ylabel='Count', 
+        figsize=[8, 6], add_bar_labels=True, hue_order=None, figname=figname
     )
       
     # Make a histogram of AC usage  
@@ -31,7 +34,9 @@ def do_elec_descriptive_stats(prefix=None, df_util=None, top_up=None):
         f'output/{prefix}_{str(fig_ctr).zfill(2)}_ACuse_countplot.png'
     )
     pf.make_countplot(
-        data=df_util, x='ac_use', hue=None, stat='count', saturation=1,palette=None, title=title, xlabel='AC use', ylabel='Count', hue_order=None, add_bar_labels=True, figsize=[8, 6], figname=figname
+        data=df_util, x='ac_use', hue=None, stat='count', saturation=1,
+        palette=None, title=title, xlabel='AC use', ylabel='Count', 
+        hue_order=None, add_bar_labels=True, figsize=[8, 6], figname=figname
     )
 
     # Make Histogram of AC usage Disaggregated by measured
@@ -42,7 +47,9 @@ def do_elec_descriptive_stats(prefix=None, df_util=None, top_up=None):
         f'_ACuse_and_measured_countplot.png'
     )
     pf.make_countplot(
-        data=df_util, x='ac_use', hue='measured', stat='count', palette=None, saturation=1, xlabel='AC use', ylabel='Count', add_bar_labels=True, hue_order=None, title=title, figsize=[8,6],
+        data=df_util, x='ac_use', hue='measured', stat='count', palette=None, 
+        saturation=1, xlabel='AC use', ylabel='Count', add_bar_labels=True, 
+        hue_order=None, title=title, figsize=[8,6],
         figname=figname
     )      
         
@@ -117,15 +124,19 @@ def do_elec_descriptive_stats(prefix=None, df_util=None, top_up=None):
         f'output/{prefix}_{str(fig_ctr).zfill(2)}_creditUsed_histogram.png'
     )
     pf.make_histogram(
-        df=df_util, x='ghs_used', hue=None, stat='percent', palette=None, alpha=1, xlabel='Credit Used (GHS)', ylabel='Freqency (%)', 
-        binrange=[0,60], bins=30, title='Histogram of Credit Used', hue_order=None, hue_labels=None, figsize=[8, 6], add_bar_labels=False, figname=figname
+        df=df_util, x='ghs_used', hue=None, stat='percent', palette=None, 
+        alpha=1, xlabel='Credit Used (GHS)', ylabel='Freqency (%)', 
+        binrange=[0,60], bins=30, title='Histogram of Credit Used', 
+        hue_order=None, hue_labels=None, figsize=[8, 6], add_bar_labels=False, 
+        figname=figname
     )
         
     # Boxplot of ghs_used   
     fig_ctr = 9
     figname = f'output/{prefix}_{str(fig_ctr).zfill(2)}_creditUsed_boxplot.png'
     pf.make_boxplot(
-        df=df_util, x=None, y='ghs_used', xtick_vals=None, xtick_labels=None, xlabel=None, ylabel='Credit Used (GHS)', figsize=[4,6],
+        df=df_util, x=None, y='ghs_used', xtick_vals=None, xtick_labels=None, 
+        xlabel=None, ylabel='Credit Used (GHS)', figsize=[4,6],
         title='Box Plot of Credit Used', figname=figname
     )
         
@@ -137,7 +148,8 @@ def do_elec_descriptive_stats(prefix=None, df_util=None, top_up=None):
     )
     pf.make_histogram(
         df=df_util, x='ghs_used', hue='ac_use', stat='percent', palette='deep',
-        alpha=1, binrange=[0,60], bins=30, xlabel='Credit Used (GHS)', hue_order=[0, 1], hue_labels=None, figsize=[8, 6], 
+        alpha=1, binrange=[0,60], bins=30, xlabel='Credit Used (GHS)', 
+        hue_order=[0, 1], hue_labels=None, figsize=[8, 6], 
         ylabel='Freqency (%)', add_bar_labels=False,
         title='Histogram of Credit Used with and without AC', figname=figname
     )
@@ -149,7 +161,8 @@ def do_elec_descriptive_stats(prefix=None, df_util=None, top_up=None):
     )
     pf.make_boxplot(
         df=df_util, x='ac_use', y='ghs_used', xtick_vals=[0, 1],
-        xlabel='AC Usage', xtick_labels=['Without AC', 'With AC'], ylabel='Credit Used (GHS)', figsize=[8, 6], 
+        xlabel='AC Usage', xtick_labels=['Without AC', 'With AC'], 
+        ylabel='Credit Used (GHS)', figsize=[8, 6], 
         title='Box Plot of Credit Used with and w/out AC', figname=figname
     )
             
@@ -161,9 +174,11 @@ def do_elec_descriptive_stats(prefix=None, df_util=None, top_up=None):
     )
     pf.make_histogram(
         df=df_util, x='ghs_used', hue='part', stat='percent', palette='deep',
-        alpha=0.5, binrange=[0, 60], bins=30, xlabel='Credit Used (GHS)', figsize=[8, 6], ylabel='Freqency (%)', 
+        alpha=0.5, binrange=[0, 60], bins=30, xlabel='Credit Used (GHS)', 
+        figsize=[8, 6], ylabel='Freqency (%)', 
         title='Histogram of Credit Used disaggregated by Part',
-        hue_order=[1, 2, 3, 4], hue_labels=None, add_bar_labels=False, figname=figname
+        hue_order=[1, 2, 3, 4], hue_labels=None, add_bar_labels=False, 
+        figname=figname
     )
             
     # Boxplot of ghs_used for part
